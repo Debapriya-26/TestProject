ed16e07ccae15174d9915043094a232acd8c18c0
Debapriya-26 <banerjee.debapriya9@gmail.com>
2020-10-07 20:29:41 +0530

    Added new Print statement

--- HelloWorld2/src/net/codejava/HelloWorld2.java
+++ HelloWorld2/src/net/codejava/HelloWorld2.java
@@ -3,8 +3,7 @@ package net.codejava;
 public class HelloWorld2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+		System.out.println("Hello World") ;
+		System.out.println("My First Java program");
 	}
-
 }
